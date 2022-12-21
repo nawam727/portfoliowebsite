0b2e3abe0b95097684599a5da7176e1fbd96e45c
Nawam Sahasra Mandara Denawakage <nawamsahasra27th@gmail.com>
2022-12-21 17:57:09 +0530

    Deleted testimonial files

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -18,7 +17,6 @@ const App = () => {
       <Experience />
       <Services />
       <Portfolio />
-      <Testimonials />
       <Contact />
       <Footer />
     </>
