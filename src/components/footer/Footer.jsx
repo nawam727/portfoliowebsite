429dd64549a6d21cb81cf007eafe757d1f987462
Nawam Sahasra Mandara Denawakage <nawamsahasra27th@gmail.com>
2022-12-22 14:00:30 +0530

    Completed footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {BsFacebook} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
 
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer>
+      <a href='#' className='footer_logo'>CODEONEDEV.LK</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://facebook.com"><BsFacebook/></a>
+        <a href="https://facebook.com"><BsInstagram/></a>
+        <a href="https://facebook.com"><BsTwitter/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small> &copy; CODEONEDEV.LK. All right reserved</small>
+      </div>
+    </footer>
   )
 }
 
