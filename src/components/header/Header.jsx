6949a2d4ca43371acccc55f65a5b7a3692283624
nawam727 <nawamdenawakage@gmail.com>
2023-10-27 09:31:38 +0530

    favicon added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Nawam Denawakage</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Fullstack Developer | DevOps Enthusiast</h5>
         <CTA />
         <HeaderSocial />
         <div className="me">
