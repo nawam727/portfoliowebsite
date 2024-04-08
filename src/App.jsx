84ffd1829588878177cd0d7caa47af3b4c85b62b
nawam727 <nawamdenawakage@gmail.com>
2024-04-08 20:11:21 +0530

    Preloader page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-import Education from './components/education/Education'
-import Articles from './components/articles/Articles'
+import React, { useState } from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Education from './components/education/Education';
+import Articles from './components/articles/Articles';
+import Preloader from './components/preloader/Preloader';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2500);
+
   return (
     <>
-      <Header />
-      <Nav />
-      <About />
-      <Education/>
-      <Articles/>
-      <Experience />
-      <Services />
-      <Portfolio />
-      <Contact />
-      <Footer />
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <>
+          <Header />
+          <Nav />
+          <About />
+          <Education />
+          <Articles />
+          <Experience />
+          <Services />
+          <Portfolio />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
